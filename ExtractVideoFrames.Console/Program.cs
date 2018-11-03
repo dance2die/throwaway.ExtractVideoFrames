7809cddf51236a0ed43a4e8dd7c2a54259adac90
Sung Kim <sungmkim80@gmail.com>
2018-11-03 01:16:44 -0400

    .NET Core didn't work so using .NET Framework 4.6.1

---
+++ ExtractVideoFrames.Console/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using MediaToolkit;
+using MediaToolkit.Model;
+using MediaToolkit.Options;
+
+namespace ExtractVideoFrames.Console
+{
+    class Program
+    {
+        private const string FILE_PATH = @".\videos\cb85.mp4";
+        private const string OUTPUT_DIRECTORY = @".\videos";
+
+        static void Main(string[] args)
+        {
+            // https://stackoverflow.com/a/49060721/4035
+            using (var engine = new Engine())
+            {
+                var mp4 = new MediaFile {Filename = FILE_PATH};
+
+                engine.GetMetadata(mp4);
+
+                var i = 0;
+                while (i < mp4.Metadata.Duration.TotalMinutes)
+                {
+                    try
+                    {
+                        var seekTime = TimeSpan.FromMinutes(i);
+                        var outputFile = new MediaFile {Filename = $"{OUTPUT_DIRECTORY}\\image-{i:00000}.jpeg"};
+                        System.Console.WriteLine($"Processing seekTime={seekTime} to outputFile=\"{outputFile}\"");
+
+                        var options = new ConversionOptions {Seek = seekTime};
+                        engine.GetThumbnail(mp4, outputFile, options);
+                        i++;
+                    }
+                    catch (Exception ex)
+                    {
+                        System.Console.WriteLine(ex);
+                    }
+                }
+            }
+        }
+    }
+}
--- ExtractVideoFrames/Program.cs
+++ ExtractVideoFrames/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using MediaToolkit;
+using MediaToolkit.Model;
+using MediaToolkit.Options;
 
 namespace ExtractVideoFrames
 {
     class Program
     {
+        private const string FILE_PATH = @".\videos\cb85.mp4";
+        private const string OUTPUT_DIRECTORY = @".\videos";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using (var engine = new Engine())
+            {
+                var mp4 = new MediaFile { Filename = FILE_PATH };
+
+                engine.GetMetadata(mp4);
+
+                var i = 0;
+                while (i < mp4.Metadata.Duration.Seconds)
+                {
+                    var options = new ConversionOptions { Seek = TimeSpan.FromSeconds(i) };
+                    var outputFile = new MediaFile { Filename = string.Format("{0}\\image-{1}.jpeg", OUTPUT_DIRECTORY, i) };
+                    engine.GetThumbnail(mp4, outputFile, options);
+                    i += 60;
+                }
+            }
         }
     }
 }
